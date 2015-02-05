c5e5ee09d27e0b7822288ff7d177bc6070be377c
Ryan Wilson-Perkin <rwilsonp@mail.uoguelph.ca>
2015-02-05 12:55:26 -0500

    Better variable names.

--- rpn.f95
+++ rpn.f95
@@ -1,61 +1,61 @@
 ! RPN
-! Translate algrbraic expressions to polish notation
+! Translate algrbraic expressions to polish_str notation
 ! Program prompts for algebraic string as input
-! Converts input to equivalent reverse polish notation string
+! Converts input to equivalent reverse polish_str notation string
 ! Limited to 40 characters of input
 !
 ! The variable names and their meanings are as follows:
-!     source     the input string, in normal algebraic form
-!     shier      array containing the hierarchy numbers of the input
-!     opstck     'operator stack': the operators from the input
-!     ohier      array containing the hierarchy numbers of the operators
-!     polish     the output string, in polish notation
+!     source_str   the input string, in normal algebraic form
+!     source_hier  array containing the hierarchy numbers of the input
+!     op_stack     operator stack: the operators from the input
+!     op_hier      array containing the hierarchy numbers of the operators
+!     polish_str   the output string, in polish_str notation
 !
-!     m          do index used in setting up shier array
-!     i          pointer to index string (source and shier)
-!     j          pointer to operator stack (opstck and ohier)
-!     k          pointer to output string (polish)
+!     m do index used in setting up source_hier array
+!     i pointer to index string (source_str and source_hier)
+!     j pointer to operator stack (op_stack and op_hier)
+!     k pointer to output string (polish_str)
 
 program rpn
 implicit none
 
 integer :: i, j, k, m
-character, dimension(40) :: opstck, polish, source
-integer, dimension(40) :: shier, ohier
+character, dimension(40) :: op_stack, polish_str, source_str
+integer, dimension(40) :: source_hier, op_hier
 
 do
     ! Initialize hierarchy arrays to all 0s
-    shier = 0
-    ohier = 0
+    source_hier = 0
+    op_hier = 0
 
     ! Initialize character arrays to all blanks
-    opstck = ' '
-    polish = ' '
+    op_stack = ' '
+    polish_str = ' '
 
-    ! Read (up to) 40 characters from input into source array
-    read (*, "(40a)") source
+    ! Read (up to) 40 characters from input into source_str array
+    read (*, "(40a)") source_str
 
     ! Determine hierarchy of characters in input stream
     do m = 1, 40
-        select case(source(m))
+        select case(source_str(m))
         case (' ')
             exit
         case ('(')
-            shier(m) = 1
+            source_hier(m) = 1
         case (')')
-            shier(m) = 2
+            source_hier(m) = 2
         case ('+', '-')
-            shier(m) = 3
+            source_hier(m) = 3
         case ('*', '/')
-            shier(m) = 4
+            source_hier(m) = 4
         case ('^')
-            shier(m) = 5
+            source_hier(m) = 5
         case default
-            shier(m) = 0
+            source_hier(m) = 0
         end select
     end do
 
-    ! Program exits when first character of input is a blank
+    ! Program exits when first character of input is ablank
     if (m .eq. 1) then
         exit
 
@@ -65,37 +65,37 @@ do
         cycle
     end if
 
-    ! Initialize start of output hierarchy smaller than possible source values
-    ohier(1) = -1
+    ! Initialize start of output hierarchy smaller than possible source_str values
+    op_hier(1) = -1
 
     ! Initialize operator stack and output string pointers
     j = 2
     k = 1
 
-    ! Analyze source hierarchy values and build output string
+    ! Analyze source_str hierarchy values and build output string
     do i = 1, m
-        if (shier(i) .eq. 0) then
-            polish(k) = source(i)
+        if (source_hier(i) .eq. 0) then
+            polish_str(k) = source_str(i)
             k = k + 1
-        else if (shier(i) .eq. 2) then
+        else if (source_hier(i) .eq. 2) then
             j = j - 1
         else
-            opstck(j) = source(i)
-            ohier(j) = shier(i)
+            op_stack(j) = source_str(i)
+            op_hier(j) = source_hier(i)
             j = j + 1
             cycle
         end if
 
         ! Pop higher values from operator hierarchy into output string
-        do while (ohier(j-1) .ge. shier(i + 1))
-            polish(k) = opstck(j-1)
+        do while (op_hier(j-1) .ge. source_hier(i + 1))
+            polish_str(k) = op_stack(j-1)
             k = k + 1
             j = j - 1
         end do
     end do
 
     ! Print output string
-    write (*, "(1x,a7,40a)") "INPUT: ", source
-    write (*, "(1x,a7,40a)") "RPN:   ", polish
+    write (*, "(1x,a7,40a)") "INPUT: ", source_str
+    write (*, "(1x,a7,40a)") "RPN:   ", polish_str
 end do
 end 
